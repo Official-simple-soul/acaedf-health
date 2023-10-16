8b0caaf2f32f99a94bfe89a2d7e903ad14821212
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2023-10-16 12:25:55 +0100

    new

--- src/components/VideoBG.jsx
+++ src/components/VideoBG.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 function VideoBackground({ source }) {
   return (
     <div className='absolute w-full'>
-      {/* Video goes here */}
       <video width='320' height='240' controls className='w-full'>
         <source src='/images/vid1.mp4' type='video/mp4' />
       </video>
